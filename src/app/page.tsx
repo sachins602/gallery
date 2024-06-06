84e687294bfe486d4b11d34d26ef0fde054c653c
sachins602 <sachinsapkota4@gmail.com>
2024-06-06 15:35:04 -0400

    simmple gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
-import Link from "next/link";
+const mockUrls = [
+  "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/29bd5eea-b25c-4db1-96ef-7e22b8962405-23a.jpg",
+  "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/63bc0488-30ad-4ab1-9950-12af9cc622a3-1t9xah.jpg",
+  "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/24575920-d901-4b15-80d4-35fb9a13f11a-fd1imo.jpg",
+  "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/19af4aa7-6967-4a22-8314-0b2e69f82611-3pnpbx.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          hello
-        </h1>
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="" />
+          </div>
+        ))}
       </div>
     </main>
   );
