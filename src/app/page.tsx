e2cae98f07d4593d9e210c195459373006f21479
sachins602 <sachinsapkota4@gmail.com>
2024-06-09 16:42:40 -0400

    added force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import { db } from "~/server/db";
+
+export const dynamic = "force-dynamic";
+
 const mockUrls = [
   "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/29bd5eea-b25c-4db1-96ef-7e22b8962405-23a.jpg",
   "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/63bc0488-30ad-4ab1-9950-12af9cc622a3-1t9xah.jpg",
@@ -10,10 +14,17 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id} className="w-48">
+            {post.name}
+          </div>
+        ))}
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} alt="" />
