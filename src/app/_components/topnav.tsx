c9e0900b00b2ff6778df03064d0b8c2222519135
sachins602 <sachinsapkota4@gmail.com>
2024-06-10 17:27:54 -0400

    add middleware

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,9 @@
+export default function TopNav() {
+  return (
+    <nav className="flex h-16 items-center justify-between bg-white">
+      <div className="flex items-center">
+        <div>sign in</div>
+      </div>
+    </nav>
+  );
+}
